a79529162f69b0b1293fc2a75b3415ff257fb580
abraidotti <alessandro.braidotti@gmail.com>
2020-01-29 10:04:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>work in progress...</p>{" "}
+    </div>
+  );
+}
+
+export default App;
