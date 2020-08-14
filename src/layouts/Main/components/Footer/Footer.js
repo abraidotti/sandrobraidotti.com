d2b3016331fe38a807ca820aa71403597dd480e8
Alessandro Braidotti <alessandro@kintyre.co>
2020-08-14 11:25:07 -0400

    add test logo and fix circleCI config

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -24,16 +24,15 @@ const Footer = props => {
         &copy;{' '}
         <Link
           component="a"
-          href="https://devias.io/"
+          href="https://sandrobraidotti.com/"
           target="_blank"
         >
-          Devias IO
+          Alessandro Braidotti
         </Link>
-        . 2019
+        . 2020
       </Typography>
       <Typography variant="caption">
-        Created with love for the environment. By designers and developers who
-        love to work together in offices!
+        Thanks for taking a look. YOU'RE breathtaking!
       </Typography>
     </div>
   );
