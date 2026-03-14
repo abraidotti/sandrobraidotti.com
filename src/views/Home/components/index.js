d2b3ce72cf1a546089b98ee9a6c9bd4391a4fba4
abAHEAD <alessandro.braidotti@ahead.com>
2026-03-14 12:18:42 -0400

    Replace React app with simple placeholder site + GitHub Actions CI/CD

--- src/views/Home/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as HomeStub } from './HomeStub';
-export { default as TodoStub } from './TodoStub';
--- src/views/Meta/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CommitList } from './CommitList';
-export { default as IssueCounter } from './IssueCounter';
-export { default as IssueList } from './IssueList';
-export { default as MetaStub } from './MetaStub';
